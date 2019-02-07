0fa842d39ee280ec5854f8978b1d52d78574d5b1
Lukasz Bialkowski <lukasz.adrian.bialkowski@gmail.com>
2019-02-07 15:15:51 +0100

    ErrorMap as Input and icon changed

--- projects/material-password-input/src/lib/password-input/password-input.component.ts
+++ projects/material-password-input/src/lib/password-input/password-input.component.ts
@@ -14,15 +14,9 @@ export class PasswordInputComponent implements OnInit {
     @Input() group: FormGroup;
     @Input() controlName: string;
     @Input() placeholder: string;
+    @Input() errorsMap;
     passwordForm: FormGroup;
     showPwd: boolean;
-    errorsMap = {
-        'required': 'COMMON.REQUIRED',
-        'pattern': 'AUTHENTICATION_NS.FORGOT_PASSWORD.PASSWORD_VALIDATION',
-        'minLength': 'AUTHENTICATION_NS.FORGOT_PASSWORD.PASSWORD_VALIDATION',
-        'invalidCredentials': 'AUTHENTICATION_NS.INVALID_CREDENTIALS',
-        'matchPassword': 'AUTHENTICATION_NS.PASSWORD_MISMATCH'
-    };
     
     ngOnInit(): void {
         this.passwordForm = this.group;
